cec03da12412cf64b6b3d164c926c0b66be773a2
Zach <shaw.zacht@gmail.com>
2022-05-11 14:47:56 -0700

    commit tree 2036d17aa4bec27d38b4086c64114c02e61a0ff0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+       "#{array[0]} and #{array[1]}"
+    else 
+        sentence = array.map do |word|
+            if word != array.last
+                word << "," 
+            else
+                "and #{word}"
+            end
+        end
+        sentence.join(" ")
+    end
 end
